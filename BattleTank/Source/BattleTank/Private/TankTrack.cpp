6e518d93496f27cf43029b027ee3df68f0f8164d
bradenaa <bradenaltstatt@gmail.com>
2018-08-24 10:54:20 -0700

    began to implement the fly-by-wire controls. curreently the forward and backwards motions are working

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -8,12 +8,6 @@
 
 void UTankTrack::SetThrottle(float Throttle)
 {
-//    auto Time = GetWorld() -> GetTimeSeconds();
-    auto Name = GetName();
-    UE_LOG(LogTemp, Warning, TEXT("%s throttle: %f"), *Name, Throttle);
-    
-    // TODO: clamp the actual throttle value so that player can not overdrive
-    
     auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
     auto ForceLocation = GetComponentLocation();
     auto TankRoot = Cast<UPrimitiveComponent> (GetOwner() -> GetRootComponent());
